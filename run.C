50a3d0b7cd6831d4ec5cb2bbb44b675139af2d61
peruzzim <peruzzim@server.fake>
2013-07-05 16:33:27 +0200

    Minor update

--- run.C
+++ run.C
@@ -5,17 +5,19 @@
   TString dir;
 
 
- dir=TString("./");
+ dir=TString("./gg_minitree_020616_data2011_feb4/");
+
  gen_templates(dir+TString("input_data.root"),"sigsig",1,"outphoton_data_sigsig.root","photoniso",5e5);
  gen_templates(dir+TString("input_data.root"),"sigbkg",1,"outphoton_data_sigbkg.root","photoniso",-1);
  gen_templates(dir+TString("input_data.root"),"bkgbkg",1,"outphoton_data_bkgbkg.root","photoniso",-1);
  gen_templates(dir+TString("input_data.root"),"standard",1,"outphoton_data_standard.root","photoniso");
-
-
-
+ gen_templates(dir+TString("input_data.root"),"standard_pixelrev",1,"outphoton_data_pixelrev.root","photoniso");
  gen_templates(dir+TString("input_data.root"),"zee",1,"outphoton_data_zee.root","photoniso",-1);
- gen_templates(dir+TString("input_data.root"),"randomcone",1,"outphoton_data_rcone.root","photoniso",5e5);
- gen_templates(dir+TString("input_data.root"),"sieiesideband",1,"outphoton_data_sieiesideband.root","photoniso",-1);
+
+
+
+// gen_templates(dir+TString("input_data.root"),"randomcone",1,"outphoton_data_rcone.root","photoniso",5e5);
+// gen_templates(dir+TString("input_data.root"),"sieiesideband",1,"outphoton_data_sieiesideband.root","photoniso",-1);
 
 
 
--- runmc.C
+++ runmc.C
@@ -4,15 +4,18 @@
   gROOT->ProcessLine(".L template_production.C+O");
   TString dir;
 
-
- dir = TString("gg_minitree_020616_mc_full2011purew_feb4/");
+  dir = TString("gg_minitree_020616_mc_full2011purew_feb4/");
 
  gen_templates(dir+TString("allmc.root"),"sigsig",0,"outphoton_allmc_sigsig.root","photoniso",5e5);
  gen_templates(dir+TString("allmc.root"),"sigbkg",0,"outphoton_allmc_sigbkg.root","photoniso",-1);
  gen_templates(dir+TString("allmc.root"),"bkgbkg",0,"outphoton_allmc_bkgbkg.root","photoniso",-1);
+ gen_templates(dir+TString("allmc.root"),"standard",0,"outphoton_allmc_standard.root","photoniso",-1);
+
+ gen_templates(dir+TString("ggjetsbox.root"),"sigsig",0,"outphoton_ggjetsbox_sigsig.root","photoniso",5e5);
+ gen_templates(dir+TString("ggjetsbox.root"),"standard",0,"outphoton_ggjetsbox_standard.root","photoniso",-1);
+
  gen_templates(dir+TString("allmc.root"),"signal",0,"outphoton_allmc_sig.root","photoniso",5e5);
  gen_templates(dir+TString("allmc.root"),"background",0,"outphoton_allmc_bkg.root","photoniso",5e5);
- gen_templates(dir+TString("allmc.root"),"standard",0,"outphoton_allmc_standard.root","photoniso",-1);
  gen_templates(dir+TString("allmc.root"),"2pgen",0,"outphoton_allmc_2pgen.root","photoniso",-1);
  gen_templates(dir+TString("allmc.root"),"1p1fbothgen",0,"outphoton_allmc_1p1fbothgen.root","photoniso",-1);
  gen_templates(dir+TString("allmc.root"),"1prcone1fgen",0,"outphoton_allmc_1prcone1fgen.root","photoniso",-1);
@@ -22,8 +25,12 @@
  gen_templates(dir+TString("allmc.root"),"sieiesideband",0,"outphoton_allmc_sieiesideband.root","photoniso",-1);
 
  gen_templates(dir+TString("DYJetsToLL_TuneZ2_M_50_7TeV_madgraph_tauola_Fall11_PU_S6_START42_V14B_v1_AODSIM.root"),"standard",0,"outphoton_dy_standard.root","photoniso",-1);
+ gen_templates(dir+TString("DYJetsToLL_TuneZ2_M_50_7TeV_madgraph_tauola_Fall11_PU_S6_START42_V14B_v1_AODSIM.root"),"standard_pixelrev",0,"outphoton_dy_pixelrev.root","photoniso",-1);
 
+ gen_templates(dir+TString("allmc.root"),"standard_2frag",0,"outphoton_allmc_standard_2frag.root","photoniso",-1);
  gen_templates(dir+TString("allmc.root"),"signal_2frag",0,"outphoton_allmc_sig_2frag.root","photoniso",5e5);
+ gen_templates(dir+TString("allmc.root"),"fragmentation",0,"outphoton_allmc_frag.root","photoniso",5e5);
+ gen_templates(dir+TString("allmc.root"),"nofragmentation",0,"outphoton_allmc_nofrag.root","photoniso",5e5);
 
 
 }
