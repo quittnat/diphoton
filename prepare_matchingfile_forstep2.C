6811dfbb47bb627cf68e1e0439f119d6df70d921
peruzzim <peruzzim@server.fake>
2013-07-23 11:43:23 +0200

    work on matching files producer macros

--- prepare_matchingfile_forstep2.C
+++ prepare_matchingfile_forstep2.C
@@ -4,6 +4,9 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+
+using namespace std;
 
 const int nclosest = 10;
 
@@ -70,52 +73,62 @@ void prepare_matchingfile_forstep2(TString matching, TString data){
 
 
    std::vector<UInt_t> lista_uuid;
+   lista_uuid.push_back(-1);
    for (Long64_t i=0;i<matchingtree->GetEntriesFast(); i++) {matchingtree->GetEntry(i); lista_uuid.push_back(matchingtree_event_fileuuid);}
    std::sort(lista_uuid.begin(),lista_uuid.end());
+   unsigned int size_beforeunique = lista_uuid.size();
    lista_uuid.erase(std::unique(lista_uuid.begin(),lista_uuid.end()),lista_uuid.end());
+   assert(lista_uuid.size()==size_beforeunique);
+   cout << lista_uuid.size() << " uuids found" << endl;
 
    std::map<long, TFile*> newmatchingfile;
    std::map<long, TTree*> newmatchingtree;
    std::map<long, TTree*> NewInputTree[2];
 
-   for (unsigned int i=0; i<lista_uuid.size(); i++){
-     TFile *newfile = new TFile(Form("matchingtree_%d.root",lista_uuid.at(i)),"recreate");
-     newmatchingfile.insert(std::pair<long,TFile*>(lista_uuid.at(i),newfile));
-     newfile->cd();
-     newmatchingtree.insert(std::pair<long,TTree*>(lista_uuid.at(i),matchingtree->CloneTree(0)));
-     for (int k=0; k<2; k++) NewInputTree[k].insert(std::pair<long,TTree*>(lista_uuid.at(i),InputTree[k]->CloneTree(0)));
-   }
-   
-   Int_t *matchingtree_index_1[2][2];
-   Int_t *matchingtree_index_2[2][2];
-   matchingtree_index_1[0][0]=matchingtree_index_sigsig_1+0;
-   matchingtree_index_1[0][1]=matchingtree_index_sigbkg_1+0;
-   matchingtree_index_1[1][0]=matchingtree_index_bkgsig_1+0;
-   matchingtree_index_1[1][1]=matchingtree_index_bkgbkg_1+0;
-   matchingtree_index_2[0][0]=matchingtree_index_sigsig_2+0;
-   matchingtree_index_2[0][1]=matchingtree_index_sigbkg_2+0;
-   matchingtree_index_2[1][0]=matchingtree_index_bkgsig_2+0;
-   matchingtree_index_2[1][1]=matchingtree_index_bkgbkg_2+0;
+   Int_t matchingtree_index_1[2][2][nclosest];
+   Int_t matchingtree_index_2[2][2][nclosest];
 
-   for (Long64_t i=0;i<matchingtree->GetEntriesFast(); i++) {
-     matchingtree->GetEntry(i);
-     for (int n1=0; n1<2; n1++) for (int n2=0; n2<2; n2++) for (int l=0; l<nclosest; l++){
-       InputTree[n1]->GetEntry(matchingtree_index_1[n1][n2][l]);
-       NewInputTree[n1][matchingtree_event_fileuuid]->Fill();
-       InputTree[n2]->GetEntry(matchingtree_index_2[n1][n2][l]);
-       NewInputTree[n2][matchingtree_event_fileuuid]->Fill();
-       matchingtree_index_1[n1][n2][l] = NewInputTree[n1][matchingtree_event_fileuuid]->GetEntriesFast();
-       matchingtree_index_2[n1][n2][l] = NewInputTree[n2][matchingtree_event_fileuuid]->GetEntriesFast();
+   UInt_t thisuuid=0;
+   int index_uuid=0;
+
+     for (Long64_t i=0;i<matchingtree->GetEntriesFast(); i++) {
+       matchingtree->GetEntry(i);
+       if (matchingtree_event_fileuuid!=thisuuid) {
+	 cout << thisuuid << endl;
+	 if (thisuuid!=0){
+	   newmatchingtree[thisuuid]->Write();
+	   for (int a=0; a<2; a++) NewInputTree[a][thisuuid]->Write(); 
+	   newmatchingfile[thisuuid]->Close();
+	 }
+	 thisuuid=matchingtree_event_fileuuid;
+	 cout << ++index_uuid << ". processing uuid " << thisuuid << endl;
+	 TFile *newfile = new TFile(Form("matchingtree_%u.root",thisuuid),"recreate");
+	 newmatchingfile.insert(std::pair<long,TFile*>(thisuuid,newfile));
+	 newfile->cd();
+	 newmatchingtree.insert(std::pair<long,TTree*>(thisuuid,matchingtree->CloneTree(0)));
+	 for (int m=0; m<2; m++) assert( NewInputTree[m].find(thisuuid)==NewInputTree[m].end()  );
+	 for (int m=0; m<2; m++) NewInputTree[m].insert(std::pair<long,TTree*>(thisuuid,InputTree[m]->CloneTree(0)));
+       }
+       for (int l=0; l<nclosest; l++){
+	 matchingtree_index_1[0][0][l]=matchingtree_index_sigsig_1[l];
+	 matchingtree_index_1[0][1][l]=matchingtree_index_sigbkg_1[l];
+	 matchingtree_index_1[1][0][l]=matchingtree_index_bkgsig_1[l];
+	 matchingtree_index_1[1][1][l]=matchingtree_index_bkgbkg_1[l];
+	 matchingtree_index_2[0][0][l]=matchingtree_index_sigsig_2[l];
+	 matchingtree_index_2[0][1][l]=matchingtree_index_sigbkg_2[l];
+	 matchingtree_index_2[1][0][l]=matchingtree_index_bkgsig_2[l];
+	 matchingtree_index_2[1][1][l]=matchingtree_index_bkgbkg_2[l];
+       }
+       for (int n1=0; n1<2; n1++) for (int n2=0; n2<2; n2++) for (int l=0; l<nclosest; l++){
+	 InputTree[n1]->GetEntry(matchingtree_index_1[n1][n2][l]);
+	 NewInputTree[n1][matchingtree_event_fileuuid]->Fill();
+	 InputTree[n2]->GetEntry(matchingtree_index_2[n1][n2][l]);
+	 NewInputTree[n2][matchingtree_event_fileuuid]->Fill();
+	 matchingtree_index_1[n1][n2][l] = NewInputTree[n1][matchingtree_event_fileuuid]->GetEntriesFast();
+	 matchingtree_index_2[n1][n2][l] = NewInputTree[n2][matchingtree_event_fileuuid]->GetEntriesFast();
+       }
+       newmatchingtree[matchingtree_event_fileuuid]->Fill();
      }
-     newmatchingtree[matchingtree_event_fileuuid]->Fill();
-   }
-
-
-   for (unsigned int i=0; i<lista_uuid.size(); i++){
-     newmatchingtree[lista_uuid.at(i)]->Write();
-     for (int a=0; a<2; a++) NewInputTree[a][lista_uuid.at(i)]->Write(); 
-     newmatchingfile[lista_uuid.at(i)]->Close();
-   }
 
 
 
